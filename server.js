c85fe37758916f73d1cc6fa20cb70e17bb4886dd
Joshua <joshua@macbook-air.home>
2025-07-27 23:50:58 -0400

    Fixing URL Update to use proper POST=Body handling

--- server.js
+++ server.js
@@ -329,9 +329,9 @@ app.get('/screen/timeout', (req, res) => {
 });
 
 // This will set the home page of the AIO Calendar
-app.post('/homescreen/:url', (req, res) =>{
+app.post('/newhomescreen', (req, res) =>{
   try {
-    let url = req.params.url;
+    let url = req.body.url;
     // Check if the file exists
     if (fs.existsSync(kioskPath))
     {
