6d3d7d3cff8fa5646a294310ea73461748f8980f
Joshua <joshua@macbook-air.home>
2025-09-30 23:30:16 -0400

    Fixing start command

--- server.py
+++ server.py
@@ -45,9 +45,9 @@ def get_brightness_path():
 def enable_motion():
     #run_com = "cp Motion.desktop " + motion_autostart_path;
     run_command("sudo systemctl enable motion.service; sudo systemctl daemon-reload; sudo systemctl start motion.service;");
-    print(f"Starting with command \n{run_com}\n");
+    print(f"Starting with command \n{run_command}\n");
     try:
-        code, _, err = run_command(run_com);
+        code, _, err = run_command(run_command);
         print(f"Motion enabled with {code}");
         if code != 0:
             print(f"Failed to enable motion.py: {err}", flush=True)
@@ -61,9 +61,9 @@ def enable_motion():
 # Stop the motion detection program
 def disable_motion():
     run_command("sudo systemctl stop motion.service; sudo systemctl disable motion.service; sudo systemctl daemon-reload;");
-    print(f"Stopping with command \n{run_com}\n");
+    print(f"Stopping with command \n{run_command}\n");
     try:
-        code, _, err = run_command(run_com);
+        code, _, err = run_command(run_command);
         print(f"Motion disabled with {code}");
         if code != 0:
             print(f"Failed to disable motion.py: {err}", flush=True)
